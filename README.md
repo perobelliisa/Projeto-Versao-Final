399159974beb8b984f9ef7da56b7e85141e6ca23
Isadora Perobelli <isagperobelli@gmail.com>
2026-03-09 15:15:19 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# Projeto-Versao-Final
- Crud de Usuários /  Login / Enviar email / Salvar mensagem / Gerar Gráfico / Gerar pdf / Cors / Token / Cookie / Rota Autenticada
+# envio-de-email
---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import fdb
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, origins="*")
+app.config.from_pyfile('config.py')
+host = app.config['DB_HOST']
+database = app.config['DB_NAME']
+user = app.config['DB_USER']
+password = app.config['DB_PASSWORD']
+
+try:
+    con = fdb.connect(host=host, database=database, user=user, password=password)
+    print('Conectado com sucesso')
+except Exception as e:
+    print('Erro')
+
+from view import *
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
